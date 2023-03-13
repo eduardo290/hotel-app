2d86e1f44cf30b44f3189c71fd84613bd76bbd55
Eduardo <eduardokno@gmail.com>
2023-03-13 14:53:45 -0500

    se agregaron los estilos al componente habitacion

--- src/components/Habitacion/index.jsx
+++ src/components/Habitacion/index.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./styles.css"
 function Habitacion({ tipo, precio, children }) {
   const [reservado, setReservado] = useState(false);
   const [imagen, setImagen] = useState(null);
