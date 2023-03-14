590779c880218bbd045afd65d3bd82cdb46f9a73
Henry Lopez Pardo <henry.lopez.4382@miremington.edu.co>
2023-03-14 00:40:16 -0500

    Erorres commit

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import PaginaPrincipal from './components/PaginaPrincipal'
 import Habitacion from './components/Habitacion'
 import RegistroCheckIn from './components/RegistroCheckIn'
 import PaginaCheckIn from './components/PaginaCheckIn'
-<<<<<<< HEAD
 import RegistroConsumoHabitacion from './components/registroConsumoHabitacion/RegistroConsumoHabitacion'
 import Comentarios from './components/Comentarios'
 import Mapa from './components/Mapa'
-=======
-import Contacto from './components/Contacto'
-
->>>>>>> 2d86e1f44cf30b44f3189c71fd84613bd76bbd55
 import Layout from "./Layout"
 import './App.css';
 
